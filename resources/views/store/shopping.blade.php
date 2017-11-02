c4ff71faf501355e820f7f7deb2487f280c622db
“blue8leo” <leo.navarro.31@gmail.com>
2017-11-01 23:07:44 -0400

    Iconos de botón

--- resources/views/store/shopping.blade.php
+++ resources/views/store/shopping.blade.php
@@ -37,7 +37,9 @@
                                         <p> <b>Precio:</b>{{$product->price}} $</p>
                                         <p>Cantidad: <input type="number" id="number" name="number" required="required" data-validate-minmax="1,100"></p> 
                                         
-                                        <button class="btn btn-success">Añadir</button>
+                                        <button class="btn btn-success">
+                                            <i class="fa fa-shopping-cart"></i> Añadir
+                                        </button>
                                     </div>
                                     @endforeach
                                 </div>
@@ -73,22 +75,45 @@
                                 
                               </tbody>
                             </table>
+                        <button class="btn btn-success pull-right" data-toggle="modal" data-target=".bs-example-modal-lg">
+                            <i class="fa fa-credit-card"></i> Pagar
+                        </button>
+                        
+                        <div class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-hidden="true">
+                            <div class="modal-dialog modal-lg">
+                              <div class="modal-content">
 
-                        <div style="position:relative;margin: 10px 10px;">
-                            <a href="https:&#x2F;&#x2F;payment.payphonetodoesposible.com&#x2F;/Pay/500/6190477d-a58c-4a97-86ce-73cfe61d5962/es/" 
-                            style="position: relative;
-                            padding: 8px 30px;
-                            border: 0;
-                            margin: 10px 1px;
-                            cursor: pointer;
-                            border-radius: 2px;
-                            text-transform: uppercase;
-                            text-decoration: none;
-                            color: rgba(255,255,255,.84) !important;
-                            transition: background-color .2s ease,box-shadow .28s cubic-bezier(.4,0,.2,1);
-                            outline: none!important;
-                            background-color:#ff9100;" onmouseover="this.style.backgroundColor='#ff8400'" onmouseout="this.style.backgroundColor='#ff9100'">Pagar con PayPhone</a>
-                        </div>     
+                                <div class="modal-header">
+                                  <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span>
+                                  </button>
+                                  <h4 class="modal-title" id="myModalLabel2">Resúmen de compra</h4>
+                                </div>
+                                <div class="modal-body">
+                                  <h4>Productos</h4>
+                                  <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
+                                  <p>Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla.</p>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
+                                    <div style="position:relative;margin: 10px 10px;">
+                                        <a href="https:&#x2F;&#x2F;payment.payphonetodoesposible.com&#x2F;/Pay/500/6190477d-a58c-4a97-86ce-73cfe61d5962/es/" 
+                                        style="position: relative;
+                                        padding: 8px 30px;
+                                        border: 0;
+                                        margin: 10px 1px;
+                                        cursor: pointer;
+                                        border-radius: 2px;
+                                        text-transform: uppercase;
+                                        text-decoration: none;
+                                        color: rgba(255,255,255,.84) !important;
+                                        transition: background-color .2s ease,box-shadow .28s cubic-bezier(.4,0,.2,1);
+                                        outline: none!important;
+                                        background-color:#ff9100;" onmouseover="this.style.backgroundColor='#ff8400'" onmouseout="this.style.backgroundColor='#ff9100'">Pagar con PayPhone</a>
+                                    </div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>     
                     </div>
                 </div>
             </div>
